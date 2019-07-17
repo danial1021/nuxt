d8de81d61f3bad05a566cfc2fba94cf18e3e1832
danial1021 <41097199+danial1021@users.noreply.github.com>
2019-07-17 17:08:09 +0900

    router
    The concept of
    routers in nuxt

--- pages/test.vue
+++ pages/test.vue
@@ -1,7 +1,39 @@
 <template>
-    <v-card>
-        <v-card-title>
-            hihihi
-        </v-card-title>
-    </v-card>
+  <v-card>
+    <v-card-title>
+      hihihi
+    </v-card-title>
+    <v-card-text>
+      <v-textarea v-model="text" />
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="test">
+        test
+      </v-btn>
+      <v-btn @click="hello">
+        hello
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      text: 'wwww'
+    }
+  },
+  methods: {
+    async test() {
+      // this.text = 'asfd'
+      const r = await this.$axios.get('/api')
+      this.text = r.data
+    },
+    async hello() {
+      // this.text = 'fdsa'
+      const r = await this.$axios.get('/api/hello')
+      this.text = r.data
+    }
+  }
+}
+</script>
